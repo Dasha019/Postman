2b54d7e9967264d34ab36070adca7a38bc8dd401
Dasha Kozyulya <w.vnt0d@gmail.com>
2023-02-20 11:24:38 +0500

    add hamcrest

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -1,8 +1,11 @@
 package ru.netology.rest;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
+
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matcher.equalTo;
 
 public class PostmanEchoTest {
@@ -15,7 +18,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equlTo("data"))
+                .body("data", equalTo("data"))
         ;
     }
 }
