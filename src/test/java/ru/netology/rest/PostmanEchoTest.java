e8d75c3a3ed86b91fc0d9cab7c33cfec1b970077
Dasha Kozyulya <w.vnt0d@gmail.com>
2023-02-20 14:08:14 +0500

    add test

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -3,9 +3,12 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.equalTo;
 
-public class PostmanEchoTest {
+class PostmanEchoTest {
+
+
     @Test
     void shouldReturnSendData() {
         given()
@@ -16,8 +19,21 @@ public class PostmanEchoTest {
                 .then()
                 .statusCode(200)
                 .body("data", equalTo("data"));
-           }
+    }
+
+    @Test
+    void shouldReturnSendDataSchema() {
+        given()
+                .baseUri("http://postman-echo.com")
+                .body("Data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("postmanecho.schema.json"));
+    }
 }
+
 
 
 
